806d25aa8ef5a9a769a7d8071808d6041c27b2d1
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-01-27 21:30:03 +0530

    1st question done

---
+++ Patterns/Pattern-1/Pattern-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    void printSquare(int n) {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
