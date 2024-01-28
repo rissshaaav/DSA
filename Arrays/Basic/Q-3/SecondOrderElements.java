7528fb53bbac33c3bda6d8d85447a7be34036d19
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-01-28 19:14:55 +0530

    3rd question of array solved

---
+++ Arrays/Basic/Q-3/SecondOrderElements.java
@@ -0,0 +1,37 @@
+public class SecondOrderElements {
+    // Driver code
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {8, 5, 9, 10, 3};
+        int[] result = solution.getSecondOrderElements(arr.length, arr);
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+    }
+}
+
+//solution
+class Solution {
+    int[] getSecondOrderElements(int n, int []a) {
+        int[] result = new int[2];
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        for(int i:a){
+            // updating second largest number
+            if(i>largest){
+                result[0] = largest;
+                largest = i;
+            } else if(i>result[0] && i<largest){
+                result[0] = i;
+            }
+
+            // updating second smallest number
+            if(i<smallest){
+                result[1] = smallest;
+                smallest = i;
+            } else if(i<result[1] && i>smallest){
+                result[1] = i;
+            }
+        }
+        return result;
+    }
+}
---
+++ Arrays/Basic/Q-3/readme.md
@@ -0,0 +1,19 @@
+# Find Second Smallest and Second Largest Element in an array
+**Problem Link:** [Second Largest & Second Smallest](https://www.codingninjas.com/studio/problems/ninja-and-the-second-order-elements_6581960?utm_source=striver&utm_medium=website&utm_campaign=codestudio_a_zcourse)
+
+You have been given an array ‘a’ of ‘n’ unique non-negative integers.
+Find the second largest and second smallest element from the array.
+Return the two elements (second largest and second smallest) as another array of size 2.
+
+### Example 1:
+Input: ‘n’ = 5, ‘a’ = [1, 2, 3, 4, 5]  
+Output: [4, 2]  
+Explanation: The second largest element is 4, and the second smallest element is 2.
+
+### Expected Time Complexity:
+O(n), Where ‘n’ is the size of an input array ‘arr’.
+
+### Constraints:
+2 ≤ 'n' ≤ 10^5  
+0 ≤ 'a'[i] ≤ 10^9  
+Time limit: 1 sec 
